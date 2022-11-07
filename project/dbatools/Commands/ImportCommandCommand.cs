8cae182d0da907de4f5f7792cc825bd1abaeca99
Chrissy LeMaire <clemaire@gmail.com>
2022-11-07 18:15:46 +0100

    move to dat

--- project/dbatools/Commands/ImportCommandCommand.cs
+++ project/dbatools/Commands/ImportCommandCommand.cs
@@ -1,6 +1,8 @@
 ﻿using System.Management.Automation;
-using System.Runtime.InteropServices;
 using File = System.IO.File;
+using System.IO;
+using System.Text;
+using System.IO.Compression;
 
 namespace Dataplat.Dbatools.Commands
 {
@@ -31,7 +33,23 @@ namespace Dataplat.Dbatools.Commands
         /// </summary>
         protected override void ProcessRecord()
         {
-            SessionState.InvokeCommand.InvokeScript(false, ScriptBlock.Create(File.ReadAllText(Path)), null, null);
+            if (Path.EndsWith("dat")) {
+                FileStream fs = File.Open(Path, FileMode.Open);
+                var stream = new DeflateStream(fs, CompressionMode.Decompress);
+                var sr = new StreamReader(stream, Encoding.UTF8);
+                SessionState.InvokeCommand.InvokeScript(false, ScriptBlock.Create(sr.ReadToEnd()), null, null);
+                sr.Close();
+                stream.Close();
+                fs.Close();
+                sr.Dispose();
+                stream.Dispose();
+                fs.Dispose();
+            }
+            else
+            {
+                SessionState.InvokeCommand.InvokeScript(false, ScriptBlock.Create(File.ReadAllText(Path)), null, null);
+            }
+
         }
 
         /// <summary>
@@ -43,3 +61,20 @@ namespace Dataplat.Dbatools.Commands
         #endregion Command Implementation
     }
 }
+/*
+ *   private static void CompressFile()
+    {
+        using FileStream originalFileStream = File.Open(OriginalFileName, FileMode.Open);
+        using FileStream compressedFileStream = File.Create(CompressedFileName);
+        using var compressor = new GZipStream(compressedFileStream, CompressionMode.Compress);
+        originalFileStream.CopyTo(compressor);
+    }
+
+    private static void DecompressFile()
+    {
+        using FileStream compressedFileStream = File.Open(CompressedFileName, FileMode.Open);
+        using FileStream outputFileStream = File.Create(DecompressedFileName);
+        using var decompressor = new GZipStream(compressedFileStream, CompressionMode.Decompress);
+        decompressor.CopyTo(outputFileStream);
+    }
+*/
