bc39da4890d10c8a74c87b140e31b706953f2480
Chrissy LeMaire <clemaire@gmail.com>
2025-07-15 12:11:01 +0200

    remove sqlclient

--- build/build.ps1
+++ build/build.ps1
@@ -156,36 +156,6 @@ $null = New-Item -ItemType Directory (Join-Path $libPath "core/lib/runtimes") -F
 $null = New-Item -ItemType Directory (Join-Path $tempPath "bogus") -Force
 $null = New-Item -ItemType Directory (Join-Path $tempdir "nuget") -Force
 
-Register-PackageSource -provider NuGet -name nugetRepository -Location https://www.nuget.org/api/v2 -Trusted -ErrorAction Ignore
-
-$ProgressPreference = "SilentlyContinue"
-
-# Install Microsoft.Identity.Client NuGet package
-$parms = @{
-    Provider         = "Nuget"
-    Destination      = "$tempdir\nuget"
-    Source           = "nugetRepository"
-    Scope            = "CurrentUser"
-    Force            = $true
-    SkipDependencies = $true
-}
-
-$parms.Name = "Microsoft.Identity.Client"
-$parms.RequiredVersion = "4.61.3.0"
-$null = Install-Package @parms
-
-$parms.Name = "Microsoft.Data.SqlClient"
-$parms.RequiredVersion = "5.1.6"
-Install-Package @parms
-
-
-# Copy DLLs to appropriate lib directories
-Copy-Item "$tempdir\nuget\Microsoft.Identity.Client.4.61.3\lib\net462\Microsoft.Identity.Client.dll" -Destination (Join-Path $libPath "desktop/lib/") -Force
-
-Copy-Item "$tempdir\nuget\Microsoft.Data.SqlClient.5.1.6\runtimes\unix\lib\net6.0\Microsoft.Data.SqlClient.dll" -Destination (Join-Path $libPath "core/lib/") -Force
-
-#Copy-Item "$tempdir\nuget\Microsoft.Identity.Client.4.61.3.0\lib\net461\Microsoft.Identity.Client.dll" -Destination (Join-Path $libPath "core/lib/") -Force
-
 # Download all required packages
 Invoke-WebRequest -Uri https://aka.ms/dacfx-msi -OutFile (Join-Path $tempPath "DacFramework.msi")
 Invoke-WebRequest -Uri https://www.nuget.org/api/v2/package/Bogus -OutFile (Join-Path $tempPath "bogus.zip")
