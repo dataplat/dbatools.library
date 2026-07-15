6b488bc47b571e67ff19ba20a9ab14984d121dfe
Chrissy LeMaire <clemaire@gmail.com>
2026-07-15 16:30:40 +0200

    Detect bound debug in message shim

--- project/dbatools/Message/MessageService.cs
+++ project/dbatools/Message/MessageService.cs
@@ -233,8 +233,10 @@ namespace Dataplat.Dbatools.Message
             bool restoreInquire = false;
 #if NET8_0_OR_GREATER
             object oldPreference = cmdlet.SessionState.PSVariable.GetValue("DebugPreference");
-            restoreInquire = oldPreference is ActionPreference preference &&
-                preference == ActionPreference.Inquire;
+            bool debugBound = cmdlet.MyInvocation.BoundParameters.TryGetValue("Debug", out object boundDebug) &&
+                LanguagePrimitives.IsTrue(boundDebug);
+            restoreInquire = debugBound || (oldPreference is ActionPreference preference &&
+                preference == ActionPreference.Inquire);
 #endif
             try
             {
