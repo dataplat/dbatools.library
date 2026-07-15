02d51d2268182bdf3e271e56e0214e7c8e413a48
Chrissy LeMaire <clemaire@gmail.com>
2026-07-15 10:37:50 +0200

    Match Get-DbaAgentJobOutputFile binding errors

--- project/dbatools.agent/Commands/GetDbaAgentJobOutputFileCommand.cs
+++ project/dbatools.agent/Commands/GetDbaAgentJobOutputFileCommand.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections;
+using System.Globalization;
 using System.Management.Automation;
 using Dataplat.Dbatools.Parameter;
 
@@ -17,7 +18,7 @@ public sealed class GetDbaAgentJobOutputFileCommand : DbaBaseCmdlet
 {
     /// <summary>Target SQL Server instances.</summary>
     [Parameter(Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, Position = 0)]
-    [ValidateNotNull]
+    [PsDbaInstanceArrayCast]
     [ValidateNotNullOrEmpty]
     public DbaInstanceParameter[] SqlInstance { get; set; } = null!;
 
@@ -127,4 +128,23 @@ $__dbatoolsModule = Get-Module -Name dbatools | Where-Object ModuleType -eq "Scr
     }
 } $SqlInstance $SqlCredential $Job $ExcludeJob $EnableException @__commonParameters 3>&1 2>&1
 """;
+}
+
+/// <summary>Reproduces an advanced function's typed-array conversion before validation.</summary>
+internal sealed class PsDbaInstanceArrayCastAttribute : ArgumentTransformationAttribute
+{
+    public override object? Transform(EngineIntrinsics engineIntrinsics, object? inputData)
+    {
+        if (inputData is null)
+            return null;
+
+        try
+        {
+            return LanguagePrimitives.ConvertTo(inputData, typeof(DbaInstanceParameter[]), CultureInfo.InvariantCulture);
+        }
+        catch (PSInvalidCastException ex)
+        {
+            throw new ArgumentTransformationMetadataException(ex.Message, ex);
+        }
+    }
 }
